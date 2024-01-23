129dcd3012b502da36d634369e66e96577422c73
Riva Almero <riva@dev.netkromsolution.com>
2024-01-23 13:05:47 +0700

    chore: type definitions

---
+++ types/Utility/Mapper.d.ts
@@ -0,0 +1,20 @@
+declare namespace Utility {
+   namespace Map {
+      type Student = {
+         grade: number
+         islam: number
+         katolik: number
+         kristen: number
+         hindu: number
+         buddha: number
+         konghucu: number
+         total: number
+         year: string
+      }
+   
+      type Teacher = {
+         subject: string
+         value: string | number
+      }
+   }
+}
