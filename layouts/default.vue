2edd944226039caa2d2ea03e3e9185f86dd76bc8
Riva Almero <riva@dev.netkromsolution.com>
2024-01-24 12:27:48 +0700

    fix: remove min height for main element

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
 <app-header class="fixed inset-x-0 top-0 z-20"></app-header>
 
-<main class="relative min-h-screen mt-[105px] p-2">
+<main class="relative mt-[105px] p-2">
    <slot/>
 </main>
 </template>
