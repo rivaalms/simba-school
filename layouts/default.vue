2c26c3678a0c866ac9e5087ac35e99bb1b0e11cf
Riva Almero <riva@dev.netkromsolution.com>
2024-01-25 08:36:57 +0700

    fix(layout): move container to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
    <app-header class="fixed inset-x-0 top-0 z-20"></app-header>
 
    <main class="relative mt-[105px] p-2">
-      <slot/>
+      <div class="container mx-auto">
+         <slot/>
+      </div>
    </main>
 
    <lazy-app-dialog></lazy-app-dialog>
--- pages/data/index.vue
+++ pages/data/index.vue
@@ -1,127 +1,125 @@
 <template>
-   <div class="container mx-auto">
-      <u-card>
-         <data-table
-            :data="(data as Utility.DataTable<Model.Data[]>)"
-            :headers="headers"
-            :page="(filter.page as number)"
-            :per-page="(filter.per_page as number)"
-            :loading="loading"
-            @update="onTableUpdate"
-         >
-            <template #filter>
-               <div class="p-4 grid gap-4">
-                  <u-form-group label="Tahun ajaran">
-                     <year-picker
-                        v-model="(filter.year as string)"
-                        :callback="() => fetchData()"
-                     />
-                  </u-form-group>
+   <u-card>
+      <data-table
+         :data="(data as Utility.DataTable<Model.Data[]>)"
+         :headers="headers"
+         :page="(filter.page as number)"
+         :per-page="(filter.per_page as number)"
+         :loading="loading"
+         @update="onTableUpdate"
+      >
+         <template #filter>
+            <div class="p-4 grid gap-4">
+               <u-form-group label="Tahun ajaran">
+                  <year-picker
+                     v-model="(filter.year as string)"
+                     :callback="() => fetchData()"
+                  />
+               </u-form-group>
 
-                  <u-form-group label="Kategori">
-                     <u-select-menu
-                        v-model="(filter.category as number)"
-                        :options="categoryOptions"
-                        value-attribute="value"
-                        searchable
-                        searchable-placeholder="Cari..."
-                        :search-attributes="['label']"
-                        @update:model-value="(val) => {
-                           fetchTypeOptions(val)
-                           fetchData()
-                        }"
-                     >
-                        <template #label>
-                           {{ displaySelectLabel(categoryOptions, (filter.category as number), 'Pilih kategori...') }}
-                        </template>
-                     </u-select-menu>
-                  </u-form-group>
-
-                  <u-form-group label="Tipe">
-                     <u-select-menu
-                        v-model="(filter.type as number)"
-                        :options="typeOptions"
-                        value-attribute="value"
-                        seearchable
-                        searchable-placeholder="Cari..."
-                        :search-attributes="['label']"
-                        :disabled="!filter.category || (filter.category as number) < 1"
-                        @update:model-value="fetchData"
-                     >
-                        <template #label>
-                           {{ displaySelectLabel(typeOptions, (filter.type as number), 'Pilih tipe...') }}
-                        </template>
-                     </u-select-menu>
-
-                     <template #help v-if="!filter.category || (filter.category as number) < 1">
-                        Pilih kategori terlebih dahulu
-                     </template>
-                  </u-form-group>
-
-                  <u-form-group label="Status">
-                     <u-select-menu
-                        v-model="(filter.status as number)"
-                        :options="statusOptions"
-                        value-attribute="value"
-                        searchable
-                        searchable-placeholder="Cari..."
-                        :search-attributes="['label']"
-                        @update:model-value="fetchData"
-                     >
-                        <template #label>
-                           <span
-                              v-if="!!filter.status"
-                              class="inline-block h-2 w-2 rounded-full flex-shrink-0"
-                              :class="statusOptionColor(filter.status as number)"
-                              aria-hidden="true"
-                           ></span>
-
-                           {{ displaySelectLabel(statusOptions, filter.status as number, 'Pilih status...') }}
-                        </template>
-                     </u-select-menu>
-                  </u-form-group>
-
-                  <div class="flex items-center justify-end">
-                     <u-button
-                        color="black"
-                        leading-icon="i-heroicons-arrow-path-16-solid"
-                        @click.stop="resetFilter"
-                     >
-                        Atur Ulang
-                     </u-button>
-                  </div>
-               </div>
-            </template>
-
-            <template #header>
-               <div class="flex items-center justify-end gap-4">
-                  <u-button
-                     icon="i-heroicons-plus-16-solid"
+               <u-form-group label="Kategori">
+                  <u-select-menu
+                     v-model="(filter.category as number)"
+                     :options="categoryOptions"
+                     value-attribute="value"
+                     searchable
+                     searchable-placeholder="Cari..."
+                     :search-attributes="['label']"
+                     @update:model-value="(val) => {
+                        fetchTypeOptions(val)
+                        fetchData()
+                     }"
                   >
-                     Tambah
+                     <template #label>
+                        {{ displaySelectLabel(categoryOptions, (filter.category as number), 'Pilih kategori...') }}
+                     </template>
+                  </u-select-menu>
+               </u-form-group>
+
+               <u-form-group label="Tipe">
+                  <u-select-menu
+                     v-model="(filter.type as number)"
+                     :options="typeOptions"
+                     value-attribute="value"
+                     seearchable
+                     searchable-placeholder="Cari..."
+                     :search-attributes="['label']"
+                     :disabled="!filter.category || (filter.category as number) < 1"
+                     @update:model-value="fetchData"
+                  >
+                     <template #label>
+                        {{ displaySelectLabel(typeOptions, (filter.type as number), 'Pilih tipe...') }}
+                     </template>
+                  </u-select-menu>
+
+                  <template #help v-if="!filter.category || (filter.category as number) < 1">
+                     Pilih kategori terlebih dahulu
+                  </template>
+               </u-form-group>
+
+               <u-form-group label="Status">
+                  <u-select-menu
+                     v-model="(filter.status as number)"
+                     :options="statusOptions"
+                     value-attribute="value"
+                     searchable
+                     searchable-placeholder="Cari..."
+                     :search-attributes="['label']"
+                     @update:model-value="fetchData"
+                  >
+                     <template #label>
+                        <span
+                           v-if="!!filter.status"
+                           class="inline-block h-2 w-2 rounded-full flex-shrink-0"
+                           :class="statusOptionColor(filter.status as number)"
+                           aria-hidden="true"
+                        ></span>
+
+                        {{ displaySelectLabel(statusOptions, filter.status as number, 'Pilih status...') }}
+                     </template>
+                  </u-select-menu>
+               </u-form-group>
+
+               <div class="flex items-center justify-end">
+                  <u-button
+                     color="black"
+                     leading-icon="i-heroicons-arrow-path-16-solid"
+                     @click.stop="resetFilter"
+                  >
+                     Atur Ulang
                   </u-button>
                </div>
-            </template>
+            </div>
+         </template>
 
-            <template #actions="{ row }">
-               <u-dropdown
-                  :items="actionMenu(row)"
+         <template #header>
+            <div class="flex items-center justify-end gap-4">
+               <u-button
+                  icon="i-heroicons-plus-16-solid"
                >
-                  <u-button
-                     color="gray"
-                     variant="ghost"
-                     icon="i-heroicons-ellipsis-vertical-20-solid"
-                  ></u-button>
+                  Tambah
+               </u-button>
+            </div>
+         </template>
 
-                  <template #delete="{ item }">
-                     <u-icon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-red-500"></u-icon>
-                     <span class="truncate text-red-500">{{ item.label }}</span>
-                  </template>
-               </u-dropdown>
-            </template>
-         </data-table>
-      </u-card>
-   </div>
+         <template #actions="{ row }">
+            <u-dropdown
+               :items="actionMenu(row)"
+            >
+               <u-button
+                  color="gray"
+                  variant="ghost"
+                  icon="i-heroicons-ellipsis-vertical-20-solid"
+               ></u-button>
+
+               <template #delete="{ item }">
+                  <u-icon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-red-500"></u-icon>
+                  <span class="truncate text-red-500">{{ item.label }}</span>
+               </template>
+            </u-dropdown>
+         </template>
+      </data-table>
+   </u-card>
 </template>
 
 <script setup lang="ts">
